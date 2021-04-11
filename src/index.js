e96e2009efb802f94414943feff0ac3eca268124
ioannaxen <ioannatxenaki@gmail.com>
2021-04-11 19:14:44 +0100

    Adding dynamic code splitting

--- src/index.js
+++ src/index.js
@@ -3,13 +3,23 @@ import makeButton from './button'
 import makeImage from './image'
 import imageUrl from './webpack-logo.jpeg'
 const getMakeBox = () => import('./box.ts')
+const setButtonStyle = color => import(`./button/themes/${color}`)
 
 const button = makeButton('Click here')
+setButtonStyle('red').then(({ default: style }) => {
+  console.log(style)
+  button.style = style
+})
 document.body.appendChild(button)
+
 button.addEventListener('click', () => {
   getMakeBox().then(({ makeBox }) => {
     const box = makeBox('Hi there')
     document.body.appendChild(box)
+  })
+
+  setButtonStyle('violet').then(({ default: style }) => {
+    button.style = style
   })
 })
 
