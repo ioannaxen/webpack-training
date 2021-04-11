0b1672d8f3060630ddcad6432358eb2bde034895
ioannaxen <ioannatxenaki@gmail.com>
2021-04-11 17:55:12 +0100

    Adding static code splitting

--- src/box.ts
+++ src/box.ts
@@ -1,9 +1,7 @@
 import './box.css'
 
-const makeBox = (boxName: string) => {
+export const makeBox = (boxName: string) => {
   const box = document.createElement('div')
   box.innerText = boxName
   return box
 }
-
-export default makeBox
--- src/index.js
+++ src/index.js
@@ -2,15 +2,18 @@ import helloWorld from './hello-world'
 import makeButton from './button'
 import makeImage from './image'
 import imageUrl from './webpack-logo.jpeg'
-import makeBox from './box.ts'
+const getMakeBox = () => import('./box.ts')
 
 const button = makeButton('Click here')
 document.body.appendChild(button)
+button.addEventListener('click', () => {
+  getMakeBox().then(({ makeBox }) => {
+    const box = makeBox('Hi there')
+    document.body.appendChild(box)
+  })
+})
 
 const image = makeImage(imageUrl)
 document.body.appendChild(image)
-
-const box = makeBox('Hi there')
-document.body.appendChild(box)
 
 console.log(helloWorld())
